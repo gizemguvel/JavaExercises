f7110c3f9d5b8247b18c4175151d21d06028ecd5
gizemguvel <gguvel@hotmail.com>
2023-05-11 18:04:51 +0300

    updating exercises

--- src/main/java/reviewExercises/Day15_02.java
+++ src/main/java/reviewExercises/Day15_02.java
@@ -2,6 +2,7 @@ package reviewExercises;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Day15_02 {
     public static void main(String[] args) {
@@ -60,6 +61,29 @@ public class Day15_02 {
             }
         }
         System.out.println("ages = " + ages);
+
+                    /*
+                         Ask user to enter a letter
+                        If the letter exists in the list convert it to “X”
+                        otherwise, add the element user entered into the list
+                     */
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter a letter");
+        String letter=input.next();
+
+        List<String> list=new ArrayList<>();
+        list.add("a");
+        list.add("r");
+        list.add("w");
+        list.add("s");
+        list.add("t");
+
+        if(list.contains(letter)){
+            list.set(list.indexOf(letter),"X");
+        }else {
+            list.add(letter);
+        } System.out.println(list);
 
 
     }
